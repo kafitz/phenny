67340d00686ae7b41c3ddcf87cc9e7d6fdc2a1a1
Kyle Fitzsimmons <kafitz22@gmail.com>
2013-02-04 21:29:47 -0500

    Fixed major issues with TVrage script
    Was allowed to fail much to easily before; scripted more intelligent
    output formatting depending on what data is available

--- modules/tvrage.py
+++ modules/tvrage.py
@@ -15,12 +15,25 @@ BULLET = u"\u2022" # bullet point text divider
 
 class TVData:
 	def __init__(self):
-		pass
+		self.Airtime = None
+		self.Classification = None
+		self.Country = None
+		self.Ended = None
+		self.Genres = None
+		self.LatestEpisode = None
+		self.Network = None
+		self.NextEpisode = None
+		self.Premiered = None
+		self.Runtime = None
+		self.ShowName = None
+		self.ShowURL = None
+		self.Started = None
+		self.Status = None
 
 def tvrage(phenny, input):
 	""".tv tv_show - Fetches info on next and last broadcasts from TVRage.com
 	"""
-	unicode_input = unicode(input)
+	unicode_input = unicode(input[4:]) # strip '.tv' command data prefix
 	search_str = unicode_input.encode('utf-8')
 	phenny.say("Searching...")
 	tv_data = TVData()
@@ -29,6 +42,7 @@ def tvrage(phenny, input):
 	# Create the search for the input tv show
 	formatted_query = quote(search_str)
 	search_url = "http://services.tvrage.com/tools/quickinfo.php?show=%s" % formatted_query
+	print search_url
 	opener = urllib2.build_opener()
 	request = urllib2.Request(search_url)
 	setattr(tv_data, 'raw_output', opener.open(request))
@@ -45,21 +59,38 @@ def tvrage(phenny, input):
 		else:
 			pass # Ignore setting attributes with non-alphabetic characters in the tag's name
 
-	def df(attribute_name):
-		return DATA_FORMAT + attribute_name + DATA_FORMAT
+	def df(attribute_data):
+		return DATA_FORMAT + attribute_data + DATA_FORMAT
 
 	# --Phenny IRC output--
-	tf = TAG_FORMAT
 	try:
-		one = tf + "Show: " + df(tv_data.ShowName[0]) + DATA_FORMAT + " (" + tv_data.Premiered[0] + ") " + DATA_FORMAT + BULLET + tf + " Status: " + df(tv_data.Status[0]) + " " + BULLET + " "
-		two = tf + "Previous Ep: " + df(tv_data.LatestEpisode[0]) + " on " + df(tv_data.LatestEpisode[2]) + " " + BULLET + tf + " Next Ep: "
-		three = df(tv_data.NextEpisode[0]) + " on " + df(tv_data.NextEpisode[2]) + " at " + df(tv_data.Airtime[0][-8:])
-		phenny_output = one + two + three
-		phenny.say(phenny_output)
+		show_name = tv_data.ShowName[0]
 	except AttributeError:
-		phenny.say("Couldn't find show TVRage, please try again.")
-	except:
-		phenny.say("Critical generic unspecified error.")
+		phenny.say("Couldn't find show on TVRage, please try again.")
+	premiered = df(tv_data.Premiered[0]) if tv_data.Premiered else ""
+	status = df(tv_data.Status[0]) if tv_data.Status else ""
+	latest_ep_id = df(tv_data.LatestEpisode[0]) if tv_data.LatestEpisode else ""
+	latest_ep_date = df(tv_data.LatestEpisode[2]) if tv_data.LatestEpisode else ""
+	next_ep_id = df(tv_data.NextEpisode[0]) if tv_data.NextEpisode else ""
+	next_ep_date = df(tv_data.NextEpisode[2]) if tv_data.NextEpisode else ""
+	ended_date = df(tv_data.Ended[0]) if tv_data.Ended else ""
+	airtime = df(tv_data.Airtime[0][-8:]) if tv_data.Airtime else ""
+
+	tf = TAG_FORMAT
+	if show_name:
+		one = tf + "Show: " + df(show_name) + DATA_FORMAT + " (" + premiered + ") " + DATA_FORMAT + BULLET + tf + " Status: " + status
+		two = " " + BULLET + " " + tf + "Previous Ep: " + latest_ep_id + " on " + latest_ep_date + " "
+		if not next_ep_id == "":
+			next_ep = BULLET + tf + " Next Ep: " + next_ep_id + " on " + next_ep_date + " at " + airtime
+			phenny_output = one + two + next_ep
+		elif not ended_date == "":
+			ended = BULLET + tf + " Ended on: " + ended_date
+			phenny_output = one + ended
+		else:
+			phenny_output = one + two
+		phenny.say(phenny_output)
+	else:
+		pass # No show found to concatenate string from
 tvrage.commands = ['tv']
 tvrage.name = 'tv'
 tvrage.example = '.tv The Wire'
