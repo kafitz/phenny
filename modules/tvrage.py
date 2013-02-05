718736076c11d1dcc70ba6e439055bdb4ae0c555
Kyle Fitzsimmons <kafitz22@gmail.com>
2013-02-04 21:45:36 -0500

    Fixed TVrage output failure
    Fixed issue where since tv_data.Ended is returned as an empty string
    from tvrage, the program displayed "Ended: blank" if a show was
    continuing but does not have the next air date specified

--- modules/tvrage.py
+++ modules/tvrage.py
@@ -35,14 +35,13 @@ def tvrage(phenny, input):
 	"""
 	unicode_input = unicode(input[4:]) # strip '.tv' command data prefix
 	search_str = unicode_input.encode('utf-8')
-	phenny.say("Searching...")
+	phenny.say("Searching... (TVRage API can be slow)")
 	tv_data = TVData()
 
 	# --TVRage API parsing--
 	# Create the search for the input tv show
 	formatted_query = quote(search_str)
 	search_url = "http://services.tvrage.com/tools/quickinfo.php?show=%s" % formatted_query
-	print search_url
 	opener = urllib2.build_opener()
 	request = urllib2.Request(search_url)
 	setattr(tv_data, 'raw_output', opener.open(request))
@@ -51,13 +50,15 @@ def tvrage(phenny, input):
 		line_list = cleaned_line.split('@') # '@' is the divisor for tvrage data tags and values
 		line_list[0] = "".join(line_list[0].split(" "))
 		if line_list[0].isalpha() is True:
-			try:
-				line_list.append(line_list[1].split("^"))
-				line_list.pop(1)
-			except: pass
-			setattr(tv_data, line_list[0], line_list[1])
-		else:
-			pass # Ignore setting attributes with non-alphabetic characters in the tag's name
+			# Make sure there actually is a value for the returned tag (e.g., an actual date for 'Ended')
+			if line_list[1] is not '':
+				try:
+					line_list.append(line_list[1].split("^"))
+					line_list.pop(1)
+				except: pass
+				setattr(tv_data, line_list[0], line_list[1])
+			else: pass
+		else: pass # Ignore setting attributes with non-alphabetic characters in the tag's name
 
 	def df(attribute_data):
 		return DATA_FORMAT + attribute_data + DATA_FORMAT
@@ -71,20 +72,20 @@ def tvrage(phenny, input):
 	status = df(tv_data.Status[0]) if tv_data.Status else ""
 	latest_ep_id = df(tv_data.LatestEpisode[0]) if tv_data.LatestEpisode else ""
 	latest_ep_date = df(tv_data.LatestEpisode[2]) if tv_data.LatestEpisode else ""
-	next_ep_id = df(tv_data.NextEpisode[0]) if tv_data.NextEpisode else ""
+	next_ep_id = df(tv_data.NextEpisode[0]) if tv_data.NextEpisode else None
 	next_ep_date = df(tv_data.NextEpisode[2]) if tv_data.NextEpisode else ""
-	ended_date = df(tv_data.Ended[0]) if tv_data.Ended else ""
+	ended_date = df(tv_data.Ended[0]) if tv_data.Ended else None
 	airtime = df(tv_data.Airtime[0][-8:]) if tv_data.Airtime else ""
 
 	tf = TAG_FORMAT
 	if show_name:
 		one = tf + "Show: " + df(show_name) + DATA_FORMAT + " (" + premiered + ") " + DATA_FORMAT + BULLET + tf + " Status: " + status
 		two = " " + BULLET + " " + tf + "Previous Ep: " + latest_ep_id + " on " + latest_ep_date + " "
-		if not next_ep_id == "":
+		if next_ep_id:
 			next_ep = BULLET + tf + " Next Ep: " + next_ep_id + " on " + next_ep_date + " at " + airtime
 			phenny_output = one + two + next_ep
-		elif not ended_date == "":
-			ended = BULLET + tf + " Ended on: " + ended_date
+		elif ended_date:
+			ended = " " + BULLET + tf + " Ended on: " + ended_date
 			phenny_output = one + ended
 		else:
 			phenny_output = one + two
